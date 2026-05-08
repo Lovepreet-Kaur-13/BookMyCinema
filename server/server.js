9c958d6fe7a627356812bbaf029bd17359363814
Lovepreet Kaur <lovepreetkaloya@gmail.com>
2026-05-08 10:44:27 +0530

    connect backened to MongoDB Atlas using dotenv

--- server/server.js
+++ server/server.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+// LOAD env VARIABES to process.env
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+connectDB();
+
 const PORT = 8081;
 
 app.listen( PORT, () =>{
