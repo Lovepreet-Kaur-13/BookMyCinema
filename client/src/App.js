5533ea13e714ee37393914faab1728df8e01a5b2
Lovepreet Kaur <lovepreetkaloya@gmail.com>
2026-05-09 15:01:51 +0530

    add basic protected route setup

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import ProtectedRoute from './components/ProtectedRoute';
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={
+          <ProtectedRoute>
+            <Home />
+            </ProtectedRoute> 
+          } />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
