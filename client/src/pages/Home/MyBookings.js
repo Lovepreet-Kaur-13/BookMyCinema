cb9e652af4bbabec28dd698c893d17db595cb619
Lovepreet Kaur <lovepreetkaloya@gmail.com>
2026-06-11 10:23:13 +0530

    improve myBooking page UI

--- client/src/pages/Home/MyBookings.js
+++ client/src/pages/Home/MyBookings.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { ShowLoading, HideLoading } from "../../redux/loaderSlice";
 import { GetAllBookings } from "../../api/bookings";
-import { Row, Col, Card, Button, message, Tag, Spin } from "antd";
+import { Row, Col, Card, Button, message, Tag, Spin, Space } from "antd";
 import moment from "moment";
 import { Link } from "react-router-dom";
 
@@ -24,10 +24,8 @@ const MyBookings = () => {
       } else {
         message.warning(response.message);
       }
-
     } catch (err) {
       message.error(err.message);
-
     } finally {
       setLoading(false);
       dispatch(HideLoading());
@@ -38,87 +36,67 @@ const MyBookings = () => {
     getData();
   }, []);
 
-  // LOADING UI
   if (loading) {
     return (
-      <div
-        style={{
-          height: "70vh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-        }}
-      >
+      <div className="min-h-screen p-4 md:p-6 lg:p-8">
         <Spin size="large" />
-        <p style={{ marginTop: 10 }}>Loading your bookings...</p>
+        <p>Loading your bookings...</p>
       </div>
     );
   }
 
   return (
-    <div className="myBookingPage">
+    <div className="min-h-screen p-4 md:p-6 lg:p-8">
       {bookings.length > 0 ? (
-        <Row gutter={[20, 20]}>
+        <Row gutter={[16, 16]}>
           {bookings.map((booking) => (
-            <Col xs={24} md={12} key={booking._id}>
-              <Card hoverable className="bookingCard">
-                <div className="cardContent">
-                  <img
-                    src={booking.show.movie.poster}
-                    alt="poster"
-                    className="poster"
-                  />
+            <Col xs={24} sm={12} md={8} key={booking._id}>
 
-                  <div style={{ flex: 1 }}>
-                    <h2>{booking.show.movie.title}</h2>
+              <Card title={booking.show.movie.title} bordered={true}>
 
-                    <p>
-                      🎭 <b>Theatre:</b> {booking.show.theatre.name}
-                    </p>
+                <p><b>Theatre:</b> {booking.show.theatre.name}</p>
 
-                    <p>
-                      📅 <b>Date:</b>{" "}
-                      {moment(booking.show.date).format("MMM Do YYYY")}
-                    </p>
+                <p>
+                  <b>Date:</b>{" "}
+                  {moment(booking.show.date).format("DD-MM-YYYY")}
+                </p>
 
-                    <p>
-                      ⏰ <b>Time:</b>{" "}
-                      {moment(booking.show.time, "HH:mm").format("hh:mm A")}
-                    </p>
+                <p>
+                  <b>Time:</b>{" "}
+                  {moment(booking.show.time, "HH:mm").format("hh:mm A")}
+                </p>
 
-                    <p>
-                      💺 <b>Seats:</b>{" "}
-                      {booking.seats.map((seat) => (
-                        <Tag color="blue" key={seat}>
-                          {seat}
-                        </Tag>
-                      ))}
-                    </p>
+                <p>
+                  <b>Seats:</b>{" "}
+                  <Space wrap>
+                    {booking.seats.map((seat) => (
+                      <Tag key={seat} color="red" style={{ fontWeight: 700 }}>
+                        {seat}
+                      </Tag>
+                    ))}
+                  </Space>
+                </p>
 
-                    <p>
-                      💰 <b>Amount:</b> ₹
-                      {booking.seats.length * booking.show.ticketPrice}
-                    </p>
+                <p>
+                  <b>Total Amount:</b> ₹
+                  {booking.seats.length * booking.show.ticketPrice}
+                </p>
 
-                    <p>
-                      🧾 <b>Booking ID:</b> {booking.transactionId}
-                    </p>
-                  </div>
-                </div>
+                <p>
+                  <b>Booking ID:</b> {booking.transactionId}
+                </p>
+
               </Card>
+
             </Col>
           ))}
         </Row>
       ) : (
-        <div className="emptyBookingContainer">
-          <h2>No Bookings Yet</h2>
-          <p>Looks like you haven't booked a movie yet.</p>
-
+        <div className="min-h-screen flex flex-col items-center justify-center p-4 md:p-6 lg:p-8 text-center">
+          <h2 >No Bookings Yet</h2>
+          <p className="mb-4">You haven't booked any movies yet.</p>
           <Link to="/">
-            <Button type="primary" size="large">
-              Start Booking
-            </Button>
+            <Button type="primary">Book Now</Button>
           </Link>
         </div>
       )}
